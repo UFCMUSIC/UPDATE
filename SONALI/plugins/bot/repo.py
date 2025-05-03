c5f3537e4cd2b6ef2862bc735e59df369216299d
UFCMUSIC <babymusic936@gmail.com>
2025-05-03 13:12:25 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -7,13 +7,13 @@ import httpx
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 start_txt = """
-✰ 𝗪ᴇʟᴄᴏᴍᴇ ᴛᴏ 𝗧ᴇᴀᴍ 𝗣ᴜʀᴠɪ 𝗥ᴇᴘᴏs ✰
+✰ 𝗪ᴇʟᴄᴏᴍᴇ ᴛᴏ 𝗧ᴇᴀᴍ 𝗨𝗡𝗢𝗙𝗙𝗖𝗜𝗔𝗟  𝗥ᴇᴘᴏs ✰
  
 ✰ 𝗥ᴇᴘᴏ ᴛᴏ 𝗡ʜɪ 𝗠ɪʟᴇɢᴀ 𝗬ʜᴀ
  
 ✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
 
-✰  @THUNDERDEVS  
+✰ @UFC_NETWORK   
  
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
  
@@ -29,21 +29,21 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔ᴅᴅ ᴍᴇ 𝗠ᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/THUNDERDEVS"),
-          InlineKeyboardButton("⍣ ፝֠֩ ‌ ‌ꭘⷪ ‌ ꯭፝֠֩‌ ꯭‌ ‌‌𝚰𝛅꯭𝗍ᴋ꯭ʜᴧ꯭ꝛ", url="https://t.me/CRAZY_OP_02"),
+          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/UFC_NETWORK"),
+          InlineKeyboardButton("ɪɴɴᴏᴄᴇɴᴛ ʙᴀᴄʜᴀ", url="https://t.me/UFC_INOCENT"),
           ],
                [
-                InlineKeyboardButton("𝗧ᴇᴀᴍ 𝗣ᴜʀᴠɪ 𝗕ᴏᴛs", url=f"https://t.me/PURVI_SUPPORT"),
+                InlineKeyboardButton("𝗧ᴇᴀᴍ 𝗨𝗡𝗢𝗙𝗙𝗖𝗜𝗔𝗟 ", url=f"https://t.me/UFC_NETWORK"),
 ],
 [
-InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/PURVI_MUSIC_BOT"),
+InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/RJ_92_MUSIC_BOT"),
 
         ]]
 
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await msg.reply_photo(
-        photo="https://files.catbox.moe/u8ih4t.mp4",
+        photo="https://files.catbox.moe/mccc35.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
