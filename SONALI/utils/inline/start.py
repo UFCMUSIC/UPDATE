7b7e3d2ef9f4c28849b430a69e6d27e8b888b1c2
UFCMUSIC <babymusic936@gmail.com>
2025-05-03 15:54:50 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -27,7 +27,7 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/UFC_NETWORK/89"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/UFC_NETWORK"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
