c5684a64393418b8e23f1c1f02fecf83d3fb76a7
UFCMUSIC <babymusic936@gmail.com>
2025-05-03 13:04:37 +0530

    Update broadcast.py

--- SONALI/plugins/misc/broadcast.py
+++ SONALI/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command(["broadcast", "gcast"]) & filters.user(7995626429))
+@app.on_message(filters.command(["broadcast", "gcast"]) & filters.user(8184370372))
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
